626bd37954eac982a82ed7d4efb2286f8c8d31b5
bhavikavasani <117728829+bhavikavasani@users.noreply.github.com>
2023-08-07 23:25:13 +0530

    Add files via upload

---
+++ src/products.js
@@ -0,0 +1,67 @@
+import product1 from "./assets/products/product-1.jpg";
+import product2 from "./assets/products/product-2.jpg";
+import product3 from "./assets/products/product-3.jpg";
+import product4 from "./assets/products/product-4.jpg";
+import product5 from "./assets/products/product-5.jpg";
+import product6 from "./assets/products/product-6.jpg";
+import product7 from "./assets/products/product-7.jpg";
+import product8 from "./assets/products/product-8.jpg";
+import product9 from "./assets/products/product-9.png"; 
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Lunch Box",
+    price: 499.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Rotable Hooks",
+    price: 199.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Ice-cream Cup",
+    price: 399.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Deer Head Fancy Key-Holder",
+    price: 299.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Soap Holder",
+    price: 190.99,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Storage Container",
+    price: 599.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Professionl Tifin-Box",
+    price: 499.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Fruit juicer",
+    price: 699.0,
+    productImage: product8,
+  },
+  {
+    id: 9,
+    productName: "Mobile Holder",
+    price: 199.0,
+    productImage: product9,
+  }
+
+];
